d30af6e6c2387b0c56ec48fb915d9ea57f19f3e0
PabloRR100 <pabloruizruiz10@gmail.com>
2018-08-15 13:33:44 -0400

    updated ResNets for CIFAR-10

--- models/resnet.py
+++ models/resnet.py
@@ -7,6 +7,7 @@ BasicBlock and Bottleneck module is from the original ResNet paper:
 PreActBlock and PreActBottleneck module is from the later paper:
 [2] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
     Identity Mappings in Deep Residual Networks. arXiv:1603.05027
+    
 '''
 import torch
 import torch.nn as nn
@@ -22,25 +23,31 @@ def conv3x3(in_planes, out_planes, stride=1):
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
+        
         self.conv1 = conv3x3(in_planes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
         self.conv2 = conv3x3(planes, planes)
+        
+        self.bn1 = nn.BatchNorm2d(planes)
         self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
-            )
-
+        
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        
+        self.stride = stride
+        
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        
+        residue = x
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
+        
+        if self.downsample is not None:
+            residue = self.downsample(x)
+            
+        out += residue
+        out = self.relu(out)
         return out
 
 
@@ -128,65 +135,84 @@ class PreActBottleneck(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, depth, name, num_classes=10):
         super(ResNet, self).__init__()
-        self.in_planes = 64
+        
+        assert (depth - 2) % 6 == 0, 'Depth should be 6n + 2'
+        n = (depth - 2) // 6
 
-        self.conv1 = conv3x3(3,64)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.name = name
+        block = BasicBlock
+        self.inplanes = 16
+        fmaps = [16, 32, 64] # CIFAR10        
+        
+        self.conv = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.layer1 = self._make_layer(block, fmaps[0], n, stride=1) 
+        self.layer2 = self._make_layer(block, fmaps[1], n, stride=2) 
+        self.layer3 = self._make_layer(block, fmaps[2], n, stride=2) 
+        
+        self.avgpool = nn.AvgPool2d(kernel_size=8, stride=1)                   
+        self.fc = nn.Linear(fmaps[2] * block.expansion, num_classes) 
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+    def _make_layer(self, block, planes, blocks, stride=1):        
+        ''' Between layers convolve input to match dimensions -> stride = 2 '''
+        
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion, 
+                              kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(planes * block.expansion))
+                
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+           
         return nn.Sequential(*layers)
 
-    def forward(self, x, lin=0, lout=5):
-        out = x
-        if lin < 1 and lout > -1:
-            out = self.conv1(out)
-            out = self.bn1(out)
-            out = F.relu(out)
-        if lin < 2 and lout > 0:
-            out = self.layer1(out)
-        if lin < 3 and lout > 1:
-            out = self.layer2(out)
-        if lin < 4 and lout > 2:
-            out = self.layer3(out)
-        if lin < 5 and lout > 3:
-            out = self.layer4(out)
-        if lout > 4:
-            out = F.avg_pool2d(out, 4)
-            out = out.view(out.size(0), -1)
-            out = self.linear(out)
-        return out
+    def forward(self, x):
+        
+        x = self.relu(self.bn(self.conv(x)))    # 32x32
+        x = self.layer1(x)                      # 32x32
+        x = self.layer2(x)                      # 16x16
+        x = self.layer3(x)                      # 8x8
 
+        x = self.avgpool(x)                     # 1x1
+        x = x.view(x.size(0), -1)               # Flatten
+        x  = self.fc(x)                         # Dense
+        return x
 
-def ResNet18():
-    return ResNet(PreActBlock, [2,2,2,2])
+def ResNet20(**kwargs):    
+    return ResNet(name = 'ResNet20', depth = 20, **kwargs)
 
-def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,3])
+def ResNet32(**kwargs):    
+    return ResNet(name = 'ResNet32', depth = 32, **kwargs)
 
-def ResNet50():
-    return ResNet(Bottleneck, [3,4,6,3])
+def ResNet44(**kwargs):    
+    return ResNet(name = 'ResNet44',depth = 44, **kwargs)
 
-def ResNet101():
-    return ResNet(Bottleneck, [3,4,23,3])
+def ResNet56(**kwargs):    
+    return ResNet(name = 'ResNet56',depth = 56, **kwargs)
 
-def ResNet152():
-    return ResNet(Bottleneck, [3,8,36,3])
+def ResNet110(**kwargs):    
+    return ResNet(name = 'ResNet110',depth = 110, **kwargs)
+
 
 
 def test():
-    net = ResNet18()
+    net = ResNet20()
     y = net(Variable(torch.randn(1,3,32,32)))
     print(y.size())
 
